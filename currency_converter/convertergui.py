4f38a3589cf73f524ddabcc976a89886b00a9a0c
tikature <detikaa10@gmail.com>
2025-06-01 20:26:01 +0700

    ADD MENU Quick Mode

--- currency_converter/convertergui.py
+++ currency_converter/convertergui.py
@@ -91,6 +91,23 @@ def get_converter_content():
                     </div>
                     <br>
                     <div class="quick-menu" id="quick-menu">
+                        <div class="quick-menu">
+                            <div class="quick-option" onclick="quickConvert('USD', 'EUR')">
+                                <h4>💵 USD to EUR</h4>
+                                <p>US Dollar to Euro</p>
+                            </div>
+                            <div class="quick-option" onclick="quickConvert('USD', 'IDR')">
+                                <h4>💵 USD to IDR</h4>
+                                <p>US Dollar to Indonesian Rupiah</p>
+                            </div>
+                            <div class="quick-option" onclick="quickConvert('USD', 'JPY')">
+                                <h4>💵 USD to JPY</h4>
+                                <p>US Dollar to Japanese Yen</p>
+                            </div>
+                            <div class="quick-option" onclick="quickConvert('IDR', 'USD')">
+                                <h4>🇮🇩 IDR to USD</h4>
+                                <p>Indonesian Rupiah to US Dollar</p>
+                            </div>
                         <!-- Quick options populated dynamically -->
                     </div>
                 </div>
