72efae362ede9c1130439bdc1b0bc0705f779c3e
zivanaa <136790171+zivanaa@users.noreply.github.com>
2025-06-03 02:21:24 +0700

    add current time

--- currency_converter/landing_page.py
+++ currency_converter/landing_page.py
@@ -5,3 +5,5 @@ def get_landing_content():
     """
     Render landing page content using HTML template
     """
+    current_time = datetime.now().strftime("%B %d, %Y")
+    return render_template("landing_page.html", current_time=current_time)
